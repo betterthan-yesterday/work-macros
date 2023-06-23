158b3e9a54aa7fe0746cf52dbe6087cd7b189a67
unknown <Administrator@mcmaster-c76500.ads.mcmaster.ca>
2023-06-23 09:50:21 -0400

    Finished photo-delete

--- photo-delete.py
+++ photo-delete.py
@@ -1,16 +1,35 @@
 import pyautogui as pag
+import time
 
 """
 Use: 16:9 fullscreen browser
 """
 
+pag.PAUSE = 1
+pag.FAILSAFE = True
+
 pag.hotkey("alt", "tab")
 
-# Get and paste student number into query
-pag.hotkey("ctrl", "c")
-pag.hotkey("ctrl", "tab")
-pag.hotkey("ctrl", "a")
-pag.hotkey("ctrl", "v")
-pag.press("enter")
+while True:
+    # Get and paste student number into query
+    pag.hotkey("ctrl", "c")
+    pag.hotkey("ctrl", "tab")
+    pag.hotkey("ctrl", "a")
+    pag.hotkey("ctrl", "v")
+    pag.press("enter")
 
-pag.mo
+    # Delete
+    time.sleep(4)
+
+    # Return
+    pag.hotkey("alt", "2")
+    pag.hotkey("ctrl", "shift", "tab")
+
+    # Record
+    pag.hotkey("ctrl", "right")
+    pag.press("right")
+    pag.write("yes")
+    pag.press("enter")
+    pag.press("left")
+    pag.hotkey("ctrl", "left")
+    pag.hotkey("ctrl", "left")
