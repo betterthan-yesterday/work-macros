d949f0300c6a8e2810de65f15b05811e0413f39d
unknown <Administrator@mcmaster-c76500.ads.mcmaster.ca>
2023-06-23 09:48:48 -0400

    Created loop for photo-review. Minor edits to guardtool-query

--- photo-review.py
+++ photo-review.py
@@ -2,15 +2,24 @@ import pyautogui as pag
 import time
 
 """
-Use: Anywhere
+Use: Windows
 """
+
+pag.PAUSE = 0.25
+pag.FAILSAFE = True
 
 # Start
 pag.hotkey("alt", "tab")
 
-pag.press("down")
 
-# Edit photo
-pag.hotkey("shift", "f10")
-pag.press("down", presses=3)
-pag.press("enter")
+while True:
+    pag.press("down")
+
+    # Edit photo
+    pag.hotkey("shift", "f10")
+    pag.press("down", presses=3)
+    pag.press("enter")
+
+    time.sleep(15)
+    pag.hotkey("alt", "tab")
+    time.sleep(1)
